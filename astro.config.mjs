00c2265bf7edef255b84cf89841a7c00ff2c561a
Armando Ochoa <armaochoa99@gmail.com>
2025-09-26 13:49:35 -0600

    ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  // 👇 Asegúrate de que estas dos líneas existan y sean correctas
+  site: 'https://www.armandoochoa.com',
+  base: '/',
   server: {
     port: 4200,
   },
